13b247a019a2e6170fa8afbbfb009114d55bb194
Oleksiy Blavat <oleksiy.blavat@verizonmedia.com>
2020-01-19 00:51:43 -0500

    rewrote all CRUD operations using Spring's built-in CRUD repository (TopicRepository)

--- src/main/java/io/javabrains/topic/TopicService.java
+++ src/main/java/io/javabrains/topic/TopicService.java
@@ -19,12 +19,6 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("java", "Core Java", "Core Java Description"),
-            new Topic("javascript", "JavaScript", "JS Description")
-    ));
-
     public List<Topic> getAllTopics() {
         //return topics;
         List<Topic> topics = new ArrayList<>();
@@ -34,33 +28,19 @@ public class TopicService {
     }
 
     public Topic getTopic(String id) {
-        Topic topic;
-        try {
-            topic = topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-        } catch (NoSuchElementException e) {
-            logger.warn(e.getMessage());
-            logger.warn("Could not find {} topic from ", id);
-            topic = new Topic("nothing", "nothing", StringUtils.capitalize(id) + " Topic Does Not Exist.");
-        }
-        return topic;
+        return topicRepository.findById(id).get();
     }
 
     public void addTopic(Topic topic) {
         //TODO: Implement a constraint to not add duplicate topics
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
-        for(int i = 0; i < topics.size(); i++){
-            Topic t = topics.get(i);
-            if(t.getId().equalsIgnoreCase(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equalsIgnoreCase(id));
+        topicRepository.deleteById(id);
     }
 }
